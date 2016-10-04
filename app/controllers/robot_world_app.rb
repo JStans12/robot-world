751eeb426e480f5b74434a70eae29844b2adedfe
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-10-04 15:27:19 -0600

    Added home page

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,11 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    erb :home
+  end
+
   get '/robots' do
     erb :index
   end
 end
-
